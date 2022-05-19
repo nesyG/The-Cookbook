b5d2eb36a20f74f1fe16104de42aaf53a349ba2e
Ines Gvoic <igvoic.work@gmail.com>
2022-05-19 13:56:13 -0700

    JS files

---
+++ js/openingSite.js
@@ -0,0 +1,29 @@
+/*const effect = document.querySelector('#fancyContainer');
+
+//function changeColor() {
+    //effect.style.backgroundColor = 'blue';
+//};
+
+//effect.addEventListener("mouseover" , changeColor);
+
+effect.addEventListener('mouseenter', changeColor);
+effect.addEventListener('mouseleave', backToBlack)
+
+function changeColor() {
+    effect.style.background = "#dbc5c0";
+    effect.style.opacity = "0.85"
+    console.log('It works')
+}
+
+function backToBlack() {
+    effect.style.background = "white";
+    effect.style.opacity = "0.4"
+    console.log('It works')
+} */
+
+/* document.querySelector('div').addEventListener('unload', transition)
+const newUser = document.querySelector('input').value
+
+function transition () {
+  document.querySelector('div').innerText = `Hello %{newUser}`
+} */
